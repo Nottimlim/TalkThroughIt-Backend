e8cf6a79c281845c6ca283551bf04318bbf6292e
Timothy Lim <contacttimothylim@gmail.com>
2024-11-04 15:27:00 -0500

    comment

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,15 +1,15 @@
-import cors from 'cors';
-
 const addCorsHeaders = (req, res, next) => {
-    // Always allow talkthroughit.netlify.app
+    // Set CORS headers
     res.header('Access-Control-Allow-Origin', 'https://talkthroughit.netlify.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma, Expires');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Max-Age', '86400'); // 24 hours
 
     // Handle preflight requests
     if (req.method === 'OPTIONS') {
-        return res.status(200).end();
+        return res.status(204).end();
     }
 
     next();
